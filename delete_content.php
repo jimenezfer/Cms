7fb1dec36c89e4fb9415ae3a1c730f9954309ca2
fox elvoret <gladstone34@protonmail.com>
2018-02-11 01:18:08 -0600

    Creating Delete Option

---
+++ delete_content.php
@@ -0,0 +1,25 @@
+<?php require_once("includes/session.php"); ?>
+<?php include_once("includes/connection.php"); //Creates connection and selects Db ?>
+<?php require_once("includes/functions.php"); //Functions folder ?>
+<?php include("includes/header.php"); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php require("includes/footer.php"); ?>
